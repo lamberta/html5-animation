588b847819ac25ed76f8f27a2e64916786fd2691
shuizhongyueming <shuizhongyueming@gmail.com>
2018-06-03 22:51:54 +0800

    新增ch05, 完成classes转换

---
+++ custom/examples/ch05/classes/arrow.js
@@ -0,0 +1,30 @@
+export default class Arrow {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.color = "#ffff00";
+    this.rotation = 0;
+  }
+  draw(context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.rotation);
+
+    context.lineWidth = 2;
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.moveTo(-50, -25);
+    context.lineTo(0, -25);
+    context.lineTo(0, -50);
+    context.lineTo(50, 0);
+    context.lineTo(0, 50);
+    context.lineTo(0, 25);
+    context.lineTo(-50, 25);
+    context.lineTo(-50, -25);
+    context.closePath();
+    context.fill();
+    context.stroke();
+
+    context.restore();
+  }
+}
