7a03bf180f6c6d8aa4926d4b70e5f3e24d52e26d
shuizhongyueming <shuizhongyueming@gmail.com>
2018-06-16 23:58:42 +0800

    新增ch09

---
+++ custom/examples/ch09/classes/box.js
@@ -0,0 +1,33 @@
+export default class Box {
+  constructor(width = 50, height = 50, color = '#ff0000') {
+    this.x = 0;
+    this.x = 0;
+    this.y = 0;
+    this.width = width;
+    this.height = height;
+    this.vx = 0;
+    this.vy = 0;
+    this.rotation = 0;
+    this.scaleX = 1;
+    this.scaleY = 1;
+    this.color = utils.parseColor(color);
+    this.lineWidth = 1;
+  }
+  draw() {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.rotation);
+    context.scale(this.scaleX, this.scaleY);
+
+    context.lineWidth = this.lineWidth;
+    context.fillStyle = this.color;
+    context.beginPath();
+    context.rect(0, 0, this.width, this.height);
+    context.closePath();
+    context.fill();
+    if (this.lineWidth > 0) {
+      context.stroke();
+    }
+    context.restore();
+  }
+}
