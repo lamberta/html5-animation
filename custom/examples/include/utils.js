54071a47a16e63b50cb7c50029e0772e9473f89c
shuizhongyueming <shuizhongyueming@gmail.com>
2018-06-21 00:04:52 +0800

    + 补充utils里面缺少的intersects方法 + 调整boxes里面相关参数

--- custom/examples/include/utils.js
+++ custom/examples/include/utils.js
@@ -171,3 +171,17 @@ export function circleContainsPoint(circle, point) {
 
   return distance <= circle.radius
 }
+
+
+/**
+ * Determine if two rectangles overlap.
+ * @param {object}  rectA Object with properties: x, y, width, height.
+ * @param {object}  rectB Object with properties: x, y, width, height.
+ * @return {boolean}
+ */
+export function intersects(rectA, rectB) {
+  return !(rectA.x + rectA.width < rectB.x ||
+           rectB.x + rectB.width < rectA.x ||
+           rectA.y + rectA.height < rectB.y ||
+           rectB.y + rectB.height < rectA.y);
+};
