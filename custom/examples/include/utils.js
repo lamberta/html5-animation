2ee7ec21f303a9703608b394d903a86eae67f64d
shuizhongyueming <shuizhongyueming@gmail.com>
2018-07-04 06:47:44 +0800

    新增colorToRGB方法改写

--- custom/examples/include/utils.js
+++ custom/examples/include/utils.js
@@ -146,6 +146,31 @@ export function parseColor (color, toNumber = false) {
 };
 
 /**
+ * Converts a color to the RGB string format: 'rgb(r,g,b)' or 'rgba(r,g,b,a)'
+ * @param {number|string} color
+ * @param {number}        alpha
+ * @return {string}
+ */
+export function colorToRGB (color, alpha) {
+  //number in octal format or string prefixed with #
+  if (typeof color === 'string' && color[0] === '#') {
+    color = window.parseInt(color.slice(1), 16);
+  }
+  alpha = (alpha === undefined) ? 1 : alpha;
+  //parse hex values
+  var r = color >> 16 & 0xff,
+      g = color >> 8 & 0xff,
+      b = color & 0xff,
+      a = (alpha < 0) ? 0 : ((alpha > 1) ? 1 : alpha);
+  //only use 'rgba' if needed
+  if (a === 1) {
+    return "rgb("+ r +","+ g +","+ b +")";
+  } else {
+    return "rgba("+ r +","+ g +","+ b +","+ a +")";
+  }
+};
+
+/**
  * Determine if a rectangle contains the coordinates (x,y) within it's boundaries.
  * @param {object}  rect  Object with properties: x, y, width, height.
  * @param {object}  point  Object with properties: x, y.
